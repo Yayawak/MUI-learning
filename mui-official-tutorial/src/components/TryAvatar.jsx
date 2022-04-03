e2b5216df6225a2a24778215cf0858989f218096
Yayawak <elio.interesante@gmail.com>
2022-04-04 00:57:45 +0700

    tried avatar comp

--- mui-official-tutorial/src/components/TryAvatar.jsx
+++ mui-official-tutorial/src/components/TryAvatar.jsx
@@ -1,13 +1,36 @@
 import React from 'react'
-import { Avatar, Stack, } from '@mui/material'
-import { Abc, AirplanemodeActive} from '@mui/icons-material'
+import { Avatar, AvatarGroup, Stack, } from '@mui/material'
+import { Abc, AirplanemodeActive, LunchDining } from '@mui/icons-material'
+import giyu from './../assets/Giyu.jpg'
+import muichiro from './../assets/Muichiro.jpg'
+import { createTheme, ThemeProvider } from '@mui/material'
+// let theme = createTheme({
+//     Avatar: {
+//         width: '200px',
+//         height: 200
+//     }
+// })
+
+const StyledAvatar = ({ children, ...props}) => (
+    <Avatar sx={{height: '70px', width: '70px' }} {...props} >
+        {children}
+    </Avatar>
+)
 const TryAvatar = () => {
     return (
         <>
-            <Avatar>
-                <Abc />
-            </Avatar>
-            <AirplanemodeActive />
+            <AvatarGroup max={4}>
+                    <StyledAvatar variant='square'>
+                        <Abc />
+                    </StyledAvatar>
+                    <AirplanemodeActive />
+                    <Avatar src={muichiro} />
+                    <StyledAvatar src={giyu} />
+                    <Avatar>
+                        <LunchDining />
+                    </Avatar>
+
+            </AvatarGroup>
         </>
     )
 }
