ff3d4477327fc1fc5fb5422fc196620e8b8c0aa4
Yayawak <elio.interesante@gmail.com>
2022-04-04 01:24:34 +0700

    learned badge

--- mui-official-tutorial/src/App.js
+++ mui-official-tutorial/src/App.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { TryAvatar } from './components/TryAvatar';
 import TryTypo from './components/TryTypo'
+import TryBadge from './components/TryBadge';
 function App() {
     return (
         <>
             {/* <TryTypo /> */}
-            <TryAvatar />
+            {/* <TryAvatar /> */}
+            {<TryBadge /> }
         </>
     );
 }
