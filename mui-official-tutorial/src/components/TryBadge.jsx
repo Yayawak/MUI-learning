b8f3631109beba61a6a5ef137d90e17ee8ba3e3c
Yayawak <elio.interesante@gmail.com>
2022-04-05 22:39:31 +0700

    changed react  rendering to be lastest update.

--- mui-official-tutorial/src/components/TryBadge.jsx
+++ mui-official-tutorial/src/components/TryBadge.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Button, Badge, createTheme, ButtonGroup, Divider, Box} from '@mui/material'
-import { Mail, ShoppingCart, Add, Remove,   } from '@mui/icons-material'
+import { Button, Badge, createTheme, ButtonGroup, Divider, Box } from '@mui/material'
+import { Mail, ShoppingCart, Add, Remove, } from '@mui/icons-material'
 
 const TryBadge = () => {
     const [count, setCount] = React.useState(1);
@@ -34,15 +34,15 @@ const TryBadge = () => {
                     </Badge>
 
                     <ButtonGroup >
-                       <Button aria-label='reduce' onClick={()=>{
-                           setCount(Math.max(count - 1, 0))
-                           }} >
+                        <Button aria-label='reduce' onClick={() => {
+                            setCount(Math.max(count - 1, 0))
+                        }} >
                             <Remove />
                         </Button>
 
-                       <Button aria-label='reduce' onClick={()=>{
-                           setCount(Math.max(count + 1, 0))
-                           }} >
+                        <Button aria-label='reduce' onClick={() => {
+                            setCount(Math.max(count + 1, 0))
+                        }} >
                             <Add />
                         </Button>
                     </ButtonGroup>
